5e659d913a4ca0c6ab67c873df8e4ffb3eeb4a23
박영하 <geoffrey.youngha.park@gmail.com>
2025-06-23 14:41:23 +0900

    [주석 추가] 프로세스 생성 1단계: STARTUPINFO 구조체 변수의 생성 및 초기화

--- wsp/예제 5-2/CreateProcess.cpp
+++ wsp/예제 5-2/CreateProcess.cpp
@@ -12,15 +12,23 @@
 int _tmain(int argc, TCHAR* argv[])
 {
 	STARTUPINFO si = { 0, };
+	// STARTUPINFO 구조체 변수를 선언과 동시에 0으로 초기화
+	// 이 변수는 CreateProcess 함수의 9번째 인자로 들어오는 요소
+	// -> ** 생성하는 프로세스의 속성을 지정하는 용도로 사용된다.
+
 	PROCESS_INFORMATION pi;
 
-	si.cb = sizeof(si);
-	si.dwFlags = STARTF_USEPOSITION | STARTF_USESIZE;
-	si.dwX = 100;
-	si.dwY = 200;
-	si.dwXSize = 300;
-	si.dwYSize = 200;
-	si.lpTitle = (wchar_t*) _T("I am a boy!"); // 형 변환: const wchar_t* -> wchar_t*
+	// Line 22-28: 생성하고자 하는 프로세스의 속성을 지정
+	si.cb = sizeof(si); // 구조체 변수의 크기
+	si.dwFlags = STARTF_USEPOSITION | STARTF_USESIZE; // 설정된 멤버의 정보
+		// STARTF_USEPOSITION: dwX, dwY 값 참조 -> 생성하는 프로세스 윈도우 위치를 결정할 때 사용
+		// STARTF_USESIZE: dwXSize, dwYSize 값 참조 -> 생성하는 프로세스 윈도우 크기를 결정할 때 사용
+
+	si.dwX = 100; // 프로세스 윈도우의 x 좌표
+	si.dwY = 200; // 프로세스 윈도우의 y 좌표
+	si.dwXSize = 300; // 프로세스 윈도우의 가로 길이
+	si.dwYSize = 200; // 프로세스 윈도우의 세로 길이
+	si.lpTitle = (wchar_t*) _T("I am a boy!"); // 형 변환: const wchar_t* -> wchar_t* // 콘솔 윈도우의 타이틀 바 제목
 
 	TCHAR command[] = _T("AdderProcess.exe 10 20");
 	TCHAR cDir[DIR_LEN];
