d62623cc9b3746821d69b4e9974aec3b69f5b83c
박영하 <geoffrey.youngha.park@gmail.com>
2025-06-24 13:56:53 +0900

    Update CloseHandlePorb1.cpp
    주석 추가

--- wsp/예제 6-4/CloseHandlePorb1.cpp
+++ wsp/예제 6-4/CloseHandlePorb1.cpp
@@ -17,9 +17,14 @@ int _tmain(int argc, TCHAR* argv[])
 	CreateProcess(
 		NULL, command, NULL, NULL, TRUE,
 		0, NULL, NULL, &si, &pi
-	);
+	); // A 프로세스는 실행과정에서 B 프로세스를 생성
 
+	// 그리고 나서 B 프로세스의 핸들을 이용해서 CloseHandle 함수를 호출한다.
+	// CloseHandle 함수의 정확한 해석: "핸들을 반환한다!"
 	CloseHandle(pi.hProcess);
+
+	// 실행 결과를 보면, 프로세스 A는 종료되지만 프로세스 B는 종료되지 않는다.
+	// 즉, CloseHandle 함수는 단순히 핸들을 반환하는 역할만을 할 뿐, 프로세스를 종료시키지는 않는다.
 
 	return 0;
 }
